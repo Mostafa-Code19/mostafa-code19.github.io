0ac943dccd399bade0081ecc2eebe49682ad85d8
Mostafa <75498261+Mostafa-Code19@users.noreply.github.com>
2021-04-11 00:41:18 +0430

    add - to query for search

--- app/script.js
+++ app/script.js
@@ -16,7 +16,7 @@ class girlGenerator {
         const key = 'PwsdNlmu1WgaOUQ2HO6iDsnAETjibM3k8T6SMNcnjuw'
         const secret = 'HSGztDfwizmDr6Hmaj8u9vzZUMgXJihEkqPXcBbJnYM'
         const src = 'https://api.unsplash.com'
-        const topic = `/search/photos?page=${this.pageNumber}&query=hot girl`
+        const topic = `/search/photos?page=${this.pageNumber}&query=hot-girl`
         const url = src + topic + '&client_id=' + key
         // const response = await fetch(url)
         const data = await response.json()
